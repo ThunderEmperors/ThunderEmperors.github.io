62ee8af8ec308615e23554ddedde9b5ebaa9e17b
ThunderEmperors <35393988+ThunderEmperors@users.noreply.github.com>
2024-02-06 18:39:19 +0530

    Add files via upload

---
+++ debsoc.js
@@ -0,0 +1,138 @@
+var white_box1 = document.getElementById("event1");
+var text1 = document.getElementById("collapse-text1");
+var img1 = document.getElementById("collapsible1");
+img1.addEventListener("click", collapse1);
+function collapse1(){
+    if(text1.style.display === "none"){
+        text1.style.display = "grid"
+        white_box1.style.width = "50%";
+    }
+    else{
+        text1.style.display = "none"
+        white_box1.style.width = "14%"
+    }
+}
+
+//
+
+var white_box2 = document.getElementById("event2");
+var text2 = document.getElementById("collapse-text2");
+var img2 = document.getElementById("collapsible2");
+img2.addEventListener("click", collapse2);
+
+
+function collapse2(){
+    if(text2.style.display === "none"){
+        text2.style.display = "grid"
+        white_box2.style.width = "50%";
+    }
+    else{
+        text2.style.display = "none"
+        white_box2.style.width = "14%"
+    }
+}
+
+//
+
+var white_box3 = document.getElementById("event3");
+var text3 = document.getElementById("collapse-text3");
+var img3 = document.getElementById("collapsible3");
+img3.addEventListener("click", collapse3);
+
+function collapse3(){
+    if(text3.style.display === "none"){
+        text3.style.display = "grid"
+        white_box3.style.width = "50%";
+    }
+    else{
+        text3.style.display = "none"
+        white_box3.style.width = "11.5%"
+    }
+}
+
+//
+
+var white_box4 = document.getElementById("event4");
+var text4 = document.getElementById("collapse-text4");
+var img4 = document.getElementById("collapsible4");
+img4.addEventListener("click", collapse4);
+function collapse4(){
+    if(text4.style.display === "none"){
+        text4.style.display = "grid"
+        white_box4.style.width = "50%";
+    }
+    else{
+        text4.style.display = "none"
+        white_box4.style.width = "12.5%"
+    }
+}
+
+//
+
+var white_box5 = document.getElementById("event5");
+var text5 = document.getElementById("collapse-text5");
+var img5 = document.getElementById("collapsible5");
+img5.addEventListener("click", collapse5);
+
+function collapse5(){
+    if(text5.style.display === "none"){
+        text5.style.display = "grid"
+        white_box5.style.width = "50%";
+    }
+    else{
+        text5.style.display = "none"
+        white_box5.style.width = "11.5%"
+    }
+}
+
+//
+
+var white_box6 = document.getElementById("event6");
+var text6 = document.getElementById("collapse-text6");
+var img6 = document.getElementById("collapsible6");
+img6.addEventListener("click", collapse6);
+
+function collapse6(){
+    if(text6.style.display === "none"){
+        text6.style.display = "grid"
+        white_box6.style.width = "60%";
+    }
+    else{
+        text6.style.display = "none"
+        white_box6.style.width = "13.8%"
+    }
+}
+
+//
+
+var white_box7 = document.getElementById("event7");
+var text7 = document.getElementById("collapse-text7");
+var img7 = document.getElementById("collapsible7");
+img7.addEventListener("click", collapse7);
+function collapse7(){
+    if(text7.style.display === "none"){
+        text7.style.display = "grid"
+        white_box7.style.width = "50%";
+    }
+    else{
+        text7.style.display = "none"
+        white_box7.style.width = "11.5%"
+    }
+}
+
+//
+
+var white_box8 = document.getElementById("event8");
+var text8 = document.getElementById("collapse-text8");
+var img8 = document.getElementById("collapsible8");
+img8.addEventListener("click", collapse8);
+function collapse8(){
+    if(text8.style.display === "none"){
+        text8.style.display = "grid"
+        white_box8.style.width = "50%";
+    }
+    else{
+        text8.style.display = "none"
+        white_box8.style.width = "11.5%"
+    }
+}
